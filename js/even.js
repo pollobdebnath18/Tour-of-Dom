d34bd2c18f10291a6bb0d2e60b7975c4140872be
pollob debnath <pullobdebnath@gmail.com>
2026-02-15 00:54:37 +0600

    setattribute , removeattribute

--- js/even.js
+++ js/even.js
@@ -24,8 +24,65 @@ document
   });
 
 //update input field
-document.getElementById('update-btn').addEventListener('click', function (){
-   const inputT = document.getElementById('input-text').value;
-   const updateName = document.getElementById('update-name');
-   updateName.innerText= inputT;
-})
+document.getElementById("update-btn").addEventListener("click", function () {
+  const inputT = document.getElementById("input-text").value;
+  const updateName = document.getElementById("update-name");
+  updateName.innerText = inputT;
+});
+
+//Comment
+document.getElementById("post-btn").addEventListener("click", function () {
+  const comment = document.getElementById("input-field");
+  const newComment = comment.value;
+  console.log(newComment);
+  const commentContainer = document.getElementById("comment-section");
+  console.log(commentContainer);
+  const newDiv = document.createElement("div");
+  newDiv.innerText = newComment;
+  commentContainer.appendChild(newDiv);
+  //clear text area
+  comment.value = "";
+});
+
+// Micky Mouse
+
+// document.getElementById('mouse').addEventListener('mouseenter', function (){
+//     console.log("Mouse Enter");
+// })
+// document.getElementById('mouse').addEventListener('mousemove', function (){
+//     console.log("Mouse Enter");
+// })
+// document.getElementById('mouse').addEventListener('mouseout', function (){
+//     console.log("Mouse Enter");
+// })
+
+// document.getElementById('input1').addEventListener('focus', function (){
+//     console.log('focus listener.');
+// })
+// document.getElementById('input2').addEventListener('blur',function(){
+//     console.log('this is blur');
+// } )
+// document.getElementById('input1').addEventListener('blur', function(){
+//     console.log('first input done')
+// })
+
+// document.getElementById('input1').addEventListener('keydown', function(event){
+//     console.log('typing : ',event.target.value);
+// })
+
+// document.getElementById('input1').addEventListener('keyup', function(event){
+//     console.log('typing : ',event.target.value);
+// })
+
+//Delete attribute
+document.getElementById("delete-item").addEventListener("keyup", function (e) {
+  const text = e.target.value;
+  const deleteBtn = document.getElementById("delete-btn");
+  if (text == "delete") {
+    console.log('yes')
+    deleteBtn.removeAttribute('disabled');
+  } else {
+    console.log('no')
+    deleteBtn.setAttribute("disabled", true);
+  }
+});
